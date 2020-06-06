9d5145e238db5dd7671ec17da11cca907ccd279c
Snigi13 <65667637+Snigi13@users.noreply.github.com>
2020-06-06 19:16:57 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,31 +4,40 @@ var deformation;
 
 
 function setup() {
-  createCanvas(1600,400);
-  var car=createSprite(400, 200, 50, 50);
-  var wall=createSprite(1500,200,50,height/2); 
-
-  speed=random(55,90);
+  createCanvas(400,400);
+  car=createSprite(50, 200, 50, 50);
+  wall=createSprite(380,200,50,400); 
   weight=random(400,1500); 
+  speed=random(55,90);
   car.velocityX=speed;
   }
 
 function draw() {
-  background(255,255,255);  
+  background(0,0,255);  
 
-  if (wall.x-car.x<(car.width + wall.width)/2){
-    car.velocityX=0;
-    deformation=0.5*weight*speed*speed/22500;
+  if (istouching(car,wall)){
+   deformation=0.5*weight*speed*speed
   } 
    if (deformation>180){
-    car.shapeColor=color(255,0,0); 
+    object1.shapeColor=color(255,0,0); 
    } 
    if (deformation<180 && deformation>100) {
-    car.shapeColor=color(230,230,0);
+    object1.shapeColor=color(230,230,0);
    } 
    if (deformation<100){
-     car.shapeColor=color(0,255,0);
+     object1.shapeColor=color(0,255,0);
    }
                                    
-  drawSprites(); 
+  drawSprites();  
+}
+
+function istouching (object1,object2){
+
+  if (object1.x-object2.x<object1.width/2+object2.width/2 &&object2.x-object1.x<object2.width/2+object1.width/2&&
+    object1.y-object2.y<object1.height/2+object2.height/2 &&object2.y-object1.y<object2.height/2+object1.height/2){  
+  return true;
+  }
+  else  {
+  return false;  
+  } 
 }
